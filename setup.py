3288097e873d7b5777e6276652ae33dd44eb0df3
Athalean Larnayé <athalean@users.noreply.github.com>
2014-04-12 01:51:25 +0200

    Delete setup.py
    everything at its time

--- setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-
-setup(name='miniblog', version='0.0.1',
-      packages=['miniblog'],
-      url='https://github.com/athalean/miniblog', license='BSD', author='Athalean', author_email='',
-      description='A static blog generator, like jekyll and hyde')
